b2248369557eff0ebdb16ba9a6bce3c975101b4e
Rich Boothe <richardjboothe@gmail.com>
2020-09-14 15:48:08 -0500

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://King:78704Coffees!@node-advanced-concepts-shard-00-00.lm8ms.mongodb.net:27017,node-advanced-concepts-shard-00-01.lm8ms.mongodb.net:27017,node-advanced-concepts-shard-00-02.lm8ms.mongodb.net:27017/Node-Advanced-Concepts?ssl=true&replicaSet=atlas-12rn21-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
